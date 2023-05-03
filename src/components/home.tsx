4ef889481fca5d9e320a9882f5f1e3fca46f0cba
mohamadreza mosalli <mohamadresaaa@gmail.com>
2023-05-03 22:35:35 +0330

    fix home component

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,8 +1,10 @@
 import { motion } from 'framer-motion'
+import { i18n, useTranslation } from 'next-i18next'
 import Image from 'next/image'
 import { useEffect, useState } from 'react'
 
 const Home = () => {
+  const { t } = useTranslation()
   const [user, setUser] = useState<any>(null)
 
   const getUser = async () => {
@@ -16,7 +18,7 @@ const Home = () => {
     setUser(response)
   }
 
-  useEffect( () => {
+  useEffect(() => {
     getUser()
     console.log('user', user)
   }, [])
@@ -53,12 +55,23 @@ const Home = () => {
         />
       </div>
       <div className='flex flex-col items-center relative mt-4'>
-        <h3 className='text-white text-xl md:text-[2rem]'>
-          Hi, I'm <span className='text-[#38efcd]'>MohamadResa</span>{' '}
+        <h3 className='text-white text-xl md:text-[2rem] capitalize'>
+          {t('sections.home.hi_im')}{' '}
+          <span className='text-[#38efcd]'>
+            {t('sections.home.mohamadresa')}
+          </span>{' '}
+          {i18n?.language === 'fa' ? 'هستم' : null}
         </h3>
-        <span className='text-white text-center text-[2.5rem] lg:text-[5rem] leading-[1.3] mt-8'>
-          Passionate
-          <br /> to craft innovative <br /> web products.
+        <span
+          className={`${
+            i18n?.language === 'fa'
+              ? 'text-[1.6rem] leading-[1.8] md:text-[2.5rem] md:leading-[1.3]'
+              : 'text-[2.5rem] leading-[1.3]'
+          } text-white text-center lg:text-[5rem] mt-8`}
+        >
+          {t('sections.home.subTitle.passionate')}
+          <br /> {t('sections.home.subTitle.to_craft_innovative')} <br />
+          {t('sections.home.subTitle.web_products')}
         </span>
         <motion.div
           whileHover={{ scale: 1.1 }}
@@ -66,7 +79,7 @@ const Home = () => {
           animate='animation'
           className='absolute capitalize hidden md:block text-gray-600 font-bold text-[0.85rem] cursor-pointer px-4 py-[0.35rem] rounded-[25px] bg-[#38efcd] right-[-1rem] top-14'
         >
-          Backend developer
+          {t('sections.home.words.backend_developer')}
         </motion.div>
         <motion.div
           whileHover={{ scale: 1.1 }}
@@ -74,7 +87,7 @@ const Home = () => {
           animate='animation'
           className='absolute capitalize hidden md:block text-gray-600 font-bold text-[0.85rem] cursor-pointer px-4 py-[0.35rem] rounded-[25px] bg-[#38efcd] lg:left-5 md:-left-5 lg:top-12 md:top-14'
         >
-          frontend developer
+          {t('sections.home.words.frontend_developer')}
         </motion.div>
         <motion.div
           whileHover={{ scale: 1.1 }}
@@ -82,7 +95,7 @@ const Home = () => {
           animate='animation'
           className='absolute capitalize hidden md:block text-gray-600 font-bold text-[0.85rem] cursor-pointer px-4 py-[0.35rem] rounded-[25px] bg-[#38efcd] left-0 -bottom-10'
         >
-          freelancer
+          {t('sections.home.words.freelancer')}
         </motion.div>
       </div>
     </motion.div>
--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -10,8 +10,10 @@ const Main = ({ user }: { user: any }) => {
     <>
       <main className='relative min-h-screen flex-col items-center justify-between p-24'>
         <div
-          className={`relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:${
-            i18n?.language === 'fa' ? 'translate-x-1/2' : '-translate-x-1/2'
+          className={`relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] ${
+            i18n?.language === 'fa'
+              ? 'before:translate-x-1/2'
+              : 'before:-translate-x-1/2'
           } before:-translate-y-1/2 before:rounded-full
       before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-2 after:translate-y-full after:bg-gradient-conic  after:blur-2xl after:content-[''] before:bg-gradient-to-br before:from-transparent before:to-blue-700/10 after:from-sky-900 after:via-[#0141ff]/40 before:lg:h-[360px]`}
         ></div>
